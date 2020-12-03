de1c7295a18c20fba818c9aca7080dcb201a5d88
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2020-12-03 03:42:21 -0700

    Display primary nav toggle only when a menu is assigned

--- header.php
+++ header.php
@@ -42,11 +42,21 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 						<?php if ( is_front_page() && is_home() ) : ?>
 
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1><small class="text-light"><?php bloginfo( 'description' ); ?></small>
+							<h1 class="navbar-brand mb-0">
+								<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url">
+									<?php bloginfo( 'name' ); ?>
+								</a>
+							</h1>
+							<small class="text-light"><?php bloginfo( 'description' ); ?></small>
 
 						<?php else : ?>
 
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a><small class="text-light"><?php bloginfo( 'description' ); ?></small>
+                            <div>
+                                <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url">
+								    <?php bloginfo( 'name' ); ?>
+							    </a>
+                            </div>
+							<small class="text-light"><?php bloginfo( 'description' ); ?></small>
 
 						<?php endif; ?>
 
@@ -55,6 +65,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 						the_custom_logo();
 					} ?><!-- end custom logo -->
 
+            <?php if ( has_nav_menu('primary') ): ?>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 					<span class="navbar-toggler-icon"></span>
 				</button>
@@ -72,9 +83,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
 					)
 				); ?>
-			<?php if ( 'container' == $container ) : ?>
+            <?php endif; ?>
+
+        <?php if ( 'container' == $container ) : ?>
 			</div><!-- .container -->
-			<?php endif; ?>
+		<?php endif; ?>
 
 		</nav><!-- .site-navigation -->
 
